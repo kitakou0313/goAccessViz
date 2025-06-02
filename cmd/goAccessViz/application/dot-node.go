44d4a0f8303e996269829b7213db2f59b229289d
kitakou0313 <kitakou0313@gmail.com>
2025-06-03 08:16:48 +0900

    add: add func to gen dot graph

--- cmd/goAccessViz/application/dot-node.go
+++ cmd/goAccessViz/application/dot-node.go
@@ -18,12 +18,27 @@ func (d *dotNode) DOTID() string {
 
 func newDotNode(node node.Node, goNumDotNode graph.Node) *dotNode {
 	return &dotNode{
-		Node:  nil,
+		Node:  goNumDotNode,
 		label: node.GetLabel(),
+	}
+}
+
+// ToDo2度同じNodeに達した場合の対応を考える
+func addDomainNodeChildrenToDotGraph(rootNode node.Node, rootDotNode *dotNode, g *simple.DirectedGraph) {
+	for _, child := range rootNode.GetChildren() {
+		childDotNode := newDotNode(child, g.NewNode())
+		g.SetEdge(g.NewEdge(rootDotNode, childDotNode))
+
+		addDomainNodeChildrenToDotGraph(child, childDotNode, g)
 	}
 }
 
 func NewDotGraph(rootNode node.Node) *simple.DirectedGraph {
 	g := simple.NewDirectedGraph()
 
+	rootDotNode := newDotNode(rootNode, g.NewNode())
+	g.AddNode(rootDotNode)
+	addDomainNodeChildrenToDotGraph(rootNode, rootDotNode, g)
+
+	return g
 }
