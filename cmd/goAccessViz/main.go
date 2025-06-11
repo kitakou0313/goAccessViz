4c3216be9630f7e2011d44751204e0ab00ebe83f
kitakou0313 <kitakou0313@gmail.com>
2025-06-12 08:21:54 +0900

    test: add test

--- cmd/goAccessViz/main.go
+++ cmd/goAccessViz/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"goAccessViz/cmd/goAccessViz/application"
 	"goAccessViz/cmd/goAccessViz/domain/node"
+	"os"
+
+	"gonum.org/v1/gonum/graph/encoding/dot"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	testNodeName1 := "testFunction1"
+	testChildrenNodes := []node.Node{
+		node.NewFunctionNode("childFunctionNode", nil),
+		node.NewDBTableNode("childDBNode", nil),
+	}
+	testRootNode1 := node.NewFunctionNode(testNodeName1, testChildrenNodes)
+	testNodeName2 := "testFunction2"
+	testRootNode2 := node.NewFunctionNode(testNodeName2, testChildrenNodes)
 
-	node := node.NewFunctionNode()
+	dotGrapth := application.NewDotGraph([]node.Node{testRootNode1, testRootNode2})
+
+	b, _ := dot.Marshal(dotGrapth, "Graph", "", " ")
+	os.WriteFile("graph.dot", b, 0644)
+
 }
